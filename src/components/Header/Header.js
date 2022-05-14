d6e5052d51cbf47f65c4f5124587187cd14c7db8
AleksandrVasilko <avasilko7@gmail.com>
2022-05-14 16:45:35 +0300

    Add folder views

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,16 @@ import UserMenu from '../UserMenu';
 import { getIsLoggedIn } from '../../redux/auth/auth-selectors';
 import { useSelector } from 'react-redux';
 
-const Header = () => { 
+const Header = () => {
     const loggedIn = useSelector(getIsLoggedIn);
     return (
         <header className={s.headerBox}>
             <nav className={s.navigationBox}>
                 <Navigation />
-                {loggedIn ? <UserMenu/> : <AuthNav/>}
+                {loggedIn ? <UserMenu /> : <AuthNav />}
             </nav>
         </header>
-    )
-}
+    );
+};
+
+export default Header;
